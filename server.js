d3d505fbdd17e49ceaeea67f79fa618e3b6b8172
weejh <wee_jh@yahoo.com>
2016-01-14 00:45:08 +0800

    Initial

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+var root = __dirname + '/app'
+app.use(express.static(root))
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('index => ', msg);
+    io.emit('chat message', msg);
+  });
+});
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
