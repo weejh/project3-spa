81b7c4c13a1b8d4d812bc8c202c4e799840c6e97
weejh <wee_jh@yahoo.com>
2016-01-14 16:08:34 +0800

    deployment 10% completed

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express()
 var http = require('http').Server(app)
-const io = require('socket.io')(http)
-const socketioJwt = require('socketio-jwt')
+// const io = require('socket.io')(https)
+// const socketioJwt = require('socketio-jwt')
 const PORT = process.env.PORT || 3000
 
 var root = __dirname + '/app'
 app.use('/', express.static(root))
 
 // enable auth0 log in from anywhere
-io.set('origins', '*domain.com*:*')
-
-io.use(socketioJwt.authorize({
-  secret: Buffer(process.env.SOCKET_SECRET, 'base64'),
-  handshake: true
-})
-)
+// io.set('origins', '*domain.com*:*')
+//
+// io.use(socketioJwt.authorize({
+//   secret: Buffer(process.env.SOCKET_SECRET, 'base64'),
+//   handshake: true
+// })
+// )
 
 http.listen(PORT, () => {
   console.log(`Listening to http://localhost:${PORT}`)
