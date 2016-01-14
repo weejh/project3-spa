2aa2a5eaaeea6ba981ebbb1b7a6da3fcf6c59d52
weejh <wee_jh@yahoo.com>
2016-01-14 13:34:41 +0800

    deployment 10% done

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 const express = require('express')
 const app = express()
 var http = require('http').Server(app)
-// var io = require('socket.io')(http);
+const io = require('socket.io')(http)
+const socketioJwt = require('socketio-jwt')
 const PORT = process.env.PORT || 3000
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
 var root = __dirname + '/app'
-// app.use(express.static(root))
 app.use('/', express.static(root))
 
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('index => ', msg);
-//     io.emit('chat message', msg);
-//   });
-// });
+io.set('origins', '*domain.com*:*')
+io.use(socketioJwt.authorize({
+  secret: Buffer(process.env.SOCKET_SECRET, 'base64'),
+  handshake: true
+}))
 
 http.listen(PORT, () => {
   console.log(`Listening to http://localhost:${PORT}`)
